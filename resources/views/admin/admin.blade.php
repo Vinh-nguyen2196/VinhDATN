c704ca86715baa817ad79d3b791b5f06c36739ef
npv34 <phanvanluan611996@gmail.com>
2019-12-19 16:08:18 +0700

    fix data table

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -32,7 +32,6 @@
 <![endif]-->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.20/datatables.min.css"/>
 
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.20/datatables.min.js"></script>
     <link rel="stylesheet" href="{{asset('css/admin.css')}}">
 </head>
 
@@ -83,4 +82,5 @@
 @jquery
 @toastr_js
 @toastr_render
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.20/datatables.min.js"></script>
 </html>
